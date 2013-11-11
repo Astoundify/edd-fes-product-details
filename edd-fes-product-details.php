21233c23b69ae7cc53d931a6770186eebd73be61
Spencer Finnell <spencerfinnell@gmail.com>
2013-11-11 11:42:19 -0500

    First pass.

---
+++ edd-fes-product-details.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * Plugin Name: Easy Digital Downloads - Frontend Submissions Product Details
+ * Plugin URI:  https://github.com/astoundify/edd-fes-product-details
+ * Description: Mark information collected during frontend submissions as "product details" to be displayed elsewhere.
+ * Author:      Astoundify
+ * Author URI:  http://astoundify.com
+ * Version:     1.0
+ */
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+class Astoundify_EDD_FPD {
+
+	/**
+	 * @var $instance
+	 */
+	private static $instance;
+
+	/**
+	 * Make sure only one instance is only running.
+	 *
+	 * @since EDD FPD 1.0
+	 *
+	 * @param void
+	 * @return object $instance The one true class instance.
+	 */
+	public static function instance() {
+		if ( ! isset ( self::$instance ) ) {
+			self::$instance = new self;
+		}
+
+		return self::$instance;
+	}
+
+	/**
+	 * Start things up.
+	 *
+	 * @since EDD FPD 1.0
+	 *
+	 * @param void
+	 * @return void
+	 */
+	public function __construct() {
+		$this->setup_globals();
+		$this->setup_actions();
+	}
+
+	/**
+	 * Set some smart defaults to class variables.
+	 *
+	 * @since EDD FPD 1.0
+	 *
+	 * @param void
+	 * @return void
+	 */
+	private function setup_globals() {
+		$this->file         = __FILE__;
+		
+		$this->basename     = plugin_basename( $this->file );
+		$this->plugin_dir   = plugin_dir_path( $this->file );
+		$this->plugin_url   = plugin_dir_url ( $this->file ); 
+	}
+
+	/**
+	 * Hooks and stuff.
+	 *
+	 * @since EDD FPD 1.0
+	 *
+	 * @param void
+	 * @return void
+	 */
+	private function setup_actions() {
+		add_action( 'edd_fes_add_field_to_common_form_element', array( $this, 'product_details' ), 100, 4 );
+		add_action( 'widgets_init', array( $this, 'widgets_init' ) );
+	}
+
+	public function widgets_init() {
+		require $this->plugin_dir . 'widget.php';
+
+		register_widget( 'EDD_FPD_Widget' );
+	}
+	
+	public function product_details( $tpl, $input_name, $id, $values ){
+		if ( 'no' == $values[ 'is_meta' ] )
+			return;
+
+		$field_name  = sprintf( $tpl, $input_name, $id, 'product_detail' );
+		$field_value = $values && isset( $values[ 'product_detail' ]) ? esc_attr( $values[ 'product_detail' ] ) : '';
+		?>
+			<div class="fes-form-rows">
+				<label><?php _e( 'Product Detail', 'edd-fpd' ); ?></label>
+				
+				<div class="fes-form-sub-fields">
+					<label for="<?php esc_attr_e( $field_name ); ?>">
+						<input type="checkbox" data-type="label" id="<?php echo esc_attr( $field_name ); ?>" name="<?php echo esc_attr( $field_name ); ?>" value="1" class="smallipopInput" <?php checked( $field_value, 1 ); ?>>
+						<?php _e( 'Show this data separately on the product listing page.', 'edd-fpd' ); ?>
+					</label>
+				</div>
+			</div><!-- .fes-form-rows -->
+		<?php
+	}
+	
+}
+add_action( 'plugins_loaded', array( 'Astoundify_EDD_FPD', 'instance' ) ); 
---
+++ widget.php
@@ -0,0 +1,185 @@
+<?php
+/**
+ * Custom Widget for product details.
+ *
+ * @link http://codex.wordpress.org/Widgets_API#Developing_Widgets
+ *
+ * @package EDD_FPD
+ * @since EDD FPD 1.0
+ */
+
+class EDD_FPD_Widget extends WP_Widget {
+
+	/**
+	 * Constructor.
+	 *
+	 * @since EDD FPD 1.0
+	 *
+	 * @return Twenty_Fourteen_Ephemera_Widget
+	 */
+	public function __construct() {
+		parent::__construct( 'widget_edd_fpd', __( 'EDD Frontend Product Details', 'edd-fpd' ), array(
+			'classname'   => 'widget_edd_fpd',
+			'description' => __( 'Use this widget to display specified product details.', 'edd-fpd' ),
+		) );
+
+		add_action( 'save_post',    array( $this, 'flush_widget_cache' ) );
+		add_action( 'deleted_post', array( $this, 'flush_widget_cache' ) );
+		add_action( 'switch_theme', array( $this, 'flush_widget_cache' ) );
+	}
+
+	/**
+	 * Output the HTML for this widget.
+	 *
+	 * @since EDD FPD 1.0
+	 *
+	 * @param array $args An array of standard parameters for widgets in this theme.
+	 * @param array $instance An array of settings for this widget instance.
+	 * @return void Echoes its output.
+	 */
+	public function widget( $args, $instance ) {
+		global $post;
+
+		// If called directly, assign an unique index for caching.
+		if ( -1 == $this->number ) {
+			static $num = -1;
+			$this->_set( --$num );
+		}
+
+		$content = get_transient( $this->id );
+
+		/*if ( false !== $content ) {
+			echo $content;
+			return;
+		}*/
+
+		$meta    = $this->get_product_details();
+
+		if ( empty( $meta ) )
+			return;
+
+		ob_start();
+		extract( $args, EXTR_SKIP );
+
+		$title  = apply_filters( 'widget_title', empty( $instance[ 'title' ] ) ? '' : $instance[ 'title' ] );		
+		echo $before_widget;
+
+		if ( '' != $title )
+			echo $before_title . $title . $after_title;
+
+		?>
+
+		<table class="edd-fpd">
+			<?php foreach ( $meta as $label => $value ) : ?>
+			<tr>
+				<th><?php echo $label; ?></th>
+				<td><?php echo $value; ?></td>
+			</tr>
+			<?php endforeach; ?>
+		</table>
+
+		<?php
+
+		echo $after_widget;
+
+		set_transient( $this->id, ob_get_flush() );
+	}
+
+	public function get_product_details() {
+		$form_id = EDD_FES()->fes_options->get_option( 'fes-submission-form' );
+		$fields  = get_post_meta( $form_id, 'fes-form', true );
+		$meta    = array();
+
+		foreach ( $fields as $field ) {
+			if ( ! isset( $field[ 'product_detail' ] ) || 'no' == $field[ 'is_meta' ] )
+				continue;
+
+			switch ( $field[ 'input_type' ] ) {
+				case 'image_upload' :
+				case 'file_upload' :
+
+					$value = get_post_meta( $post->ID, $field[ 'name' ] );
+
+					foreach ( $value as $attachment_id ) {
+						if ( 'image_upload' == $attr[ 'input_type' ] ) {
+							$thumb = wp_get_attachment_image( $attachment_id, 'thumbnail' );
+						} else {
+							$thumb = get_post_field( 'post_title', $attachment_id );
+						}
+
+						$full_size = wp_get_attachment_url( $attachment_id );
+                        $value     = sprintf( '<a href="%s">%s</a> ', $full_size, $thumb );
+					}
+
+					$meta[ $thumb ] = $value;
+
+					break;
+
+				case 'checkbox' :
+
+					$value = get_post_meta( $post->ID, $field[ 'name' ], true );
+					$value = explode( '|', $value );
+					$value = array_map( 'trim', $value );
+					$value = implode( ', ', $value );
+
+					$meta[ $field[ 'label' ] ] = $value;
+
+					break;
+
+				default :
+
+					$meta[ $field[ 'label' ] ] = get_post_meta( $post->ID, $field[ 'name' ], true );
+
+					break;
+			}
+			
+		}
+
+		return $meta;
+	}
+
+	/**
+	 * Deal with the settings when they are saved by the admin. Here is where
+	 * any validation should happen.
+	 *
+	 * @since EDD FPD 1.0
+	 *
+	 * @param array $new_instance
+	 * @param array $instance
+	 * @return array
+	 */
+	function update( $new_instance, $instance ) {
+		$instance['title']  = strip_tags( $new_instance['title'] );
+
+		$this->flush_widget_cache();
+
+		return $instance;
+	}
+
+	/**
+	 * Delete the transient.
+	 *
+	 * @since EDD FPD 1.0
+	 *
+	 * @return void
+	 */
+	function flush_widget_cache() {
+		delete_transient( $this->id );
+	}
+
+	/**
+	 * Display the form for this widget on the Widgets page of the Admin area.
+	 *
+	 * @since EDD FPD 1.0
+	 *
+	 * @param array $instance
+	 * @return void
+	 */
+	function form( $instance ) {
+		$title  = empty( $instance['title'] ) ? '' : esc_attr( $instance['title'] );
+		?>
+			<p><label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php _e( 'Title:', 'edd-fpd' ); ?></label>
+			<input id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" class="widefat" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" /></p>
+		<?php
+	}
+}
